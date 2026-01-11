0c78ce3f3fcd96f85e97c14cc256b8da138d4922
qiaobaaa <49155648+qiaobaaa@users.noreply.github.com>
2026-01-12 01:06:44 +0800

    Add files via upload

---
+++ src/Arduino_GFX_Library.h
@@ -0,0 +1,15 @@
+#ifndef _ARDUINO_GFX_LIBRARIES_H_
+#define _ARDUINO_GFX_LIBRARIES_H_
+
+#include "Arduino_DataBus.h"
+#include "databus/Arduino_ESP32DSIPanel.h"
+
+#include "Arduino_GFX.h" // Core graphics library
+#include "display/Arduino_DSI_Display.h"
+
+#include "config/displays_config.h"
+
+//Arduino_DataBus *create_default_Arduino_DataBus();
+//Arduino_GFX *create_default_Arduino_GFX();
+
+#endif // _ARDUINO_GFX_LIBRARIES_H_
